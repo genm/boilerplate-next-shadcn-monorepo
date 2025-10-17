315056c6527208284726169d8dd0c77ae95495b3
genm <7888351+genm@users.noreply.github.com>
2025-10-17 14:05:24 +0900

    chore: update ESLint configuration to include TypeScript support
    - Extend ESLint configuration to include @typescript-eslint recommended rules
    - Maintain existing configurations for recommended ESLint and Prettier

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["eslint:recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
